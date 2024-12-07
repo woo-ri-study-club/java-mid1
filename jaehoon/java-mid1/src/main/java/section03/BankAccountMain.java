8b4ec83edd3d1efe1650afbf148dc2e1a08418e5
ljh468 <ljh468@naver.com>
2024-12-07 20:29:33 +0900

    feat. 섹션03 미션 제출

---
+++ jaehoon/java-mid1/src/main/java/section03/BankAccountMain.java
@@ -0,0 +1,14 @@
+package section03;
+
+public class BankAccountMain {
+
+  public static void main(String[] args) {
+    BankAccount account1 = new BankAccount("123-456", "이재훈", 1000.0);
+    BankAccount account2 = account1.deposit(500.0);
+    BankAccount account3 = account2.withdraw(200.0);
+
+    System.out.println(account1.getBalance()); // 1000.0
+    System.out.println(account2.getBalance()); // 1500.0
+    System.out.println(account3.getBalance()); // 1300.0
+  }
+}
