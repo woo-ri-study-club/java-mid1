c23f193caf170009cc7d26fc85dec61d3d583d8b
hmkim199 <hmkim199@gmail.com>
2024-12-07 19:04:24 +0900

    feat. 미션2 제출

---
+++ hyemin/java-mid1/src/section3/BankAccount.java
@@ -0,0 +1,32 @@
+package src.section3;
+
+public class BankAccount {
+    private final String accountNumber;
+    private final String owner;
+    private final double balance;
+
+    public BankAccount(String accountNumber, String owner, double balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public BankAccount deposit(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("잘못된 금액입니다. (음수)");
+        }
+        return new BankAccount(accountNumber, owner, balance + amount);
+    }
+
+    public BankAccount withdraw(double amount) {
+        double price = balance - amount;
+        if (price < 0) {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
+        return new BankAccount(accountNumber, owner, balance - amount);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
