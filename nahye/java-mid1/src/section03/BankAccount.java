d7c443775fadfd5eb8ef885c1c5ae5d4cfc54c49
nahye <nnhh.shine@gmail.com>
2024-12-08 18:37:05 +0900

    fix. 코드리뷰 반영 by 양성빈
    - withdraw()메서드의 파라미터 예외처리 추가
    - equals() hashCode() 메서드 추가

--- nahye/java-mid1/src/section03/BankAccount.java
+++ nahye/java-mid1/src/section03/BankAccount.java
@@ -1,5 +1,7 @@
 package section03;
 
+import java.util.Objects;
+
 public class BankAccount {
     private final String accountNumber;
     private final String owner;
@@ -11,6 +13,10 @@ public class BankAccount {
         this.balance = balance;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
     public BankAccount deposit(double amount) {
         if(amount <= 0) {
             throw new IllegalArgumentException("입금액은 0보다 커야합니다.");
@@ -19,13 +25,24 @@ public class BankAccount {
     }
 
     public BankAccount withdraw(double amount) {
+        if(amount <= 0) {
+            throw new IllegalArgumentException("출금액은 0보다 커야합니다.");
+        }
         if(balance < amount) {
             throw new IllegalArgumentException("잔액이 부족합니다.");
         }
         return new BankAccount(accountNumber, owner, balance - amount);
     }
 
-    public double getBalance() {
-        return balance;
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount that = (BankAccount) o;
+        return Objects.equals(accountNumber, that.accountNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(accountNumber);
     }
 }
