fbfc3db4785509310ab89ae632c7cbcabcd3ae37
chaewonJo <ch730268@gmail.com>
2024-12-10 00:10:01 +0900

    fix.공백 무시 메서드 오류 수정

--- chaewon/java-mid1/src/section4/Mission1.java
+++ chaewon/java-mid1/src/section4/Mission1.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class Mission1 {
     public static void main(String[] args) {
-        String str1 = "Listen";
+        String str1 = "Lis  ten";
         String str2 = "Silent";
 
         System.out.println("애너그램 여부: " + isAnagram(str1, str2));
     }
 
     private static boolean isAnagram(String str1, String str2) {
-        char[] str1Array = str1.strip().toLowerCase().toCharArray();
-        char[] str2Array = str2.strip().toLowerCase().toCharArray();
+        char[] str1Array = str1.replaceAll("\\s", "").toLowerCase().toCharArray();
+        char[] str2Array = str2.replaceAll("\\s", "").toLowerCase().toCharArray();
 
         Arrays.sort(str1Array);
         Arrays.sort(str2Array);
