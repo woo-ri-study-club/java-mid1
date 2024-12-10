3cf0b7a2bc76cf12397ed1a3a1012a7d834ef18e
nahye <nnhh.shine@gmail.com>
2024-12-10 20:07:20 +0900

    fix. 미션 4의 문제1번 로직 수정 및 피드백 반영 by 성빈님, 채원님
    - 공백 처리 추가
    - 입력값에 대한 검사 추가
    - Arrays.equals() 제거 후 알파벳 배열 추가

--- nahye/java-mid1/src/section04/Quiz_1.java
+++ nahye/java-mid1/src/section04/Quiz_1.java
@@ -1,11 +1,13 @@
 package section04;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Quiz_1 {
     public static void main(String[] args) {
-        String str1 = "Listen";
+        String str1 = "Liste  n";
         String str2 = "Silent";
 
         boolean result = isAnagram(str1, str2);
@@ -13,18 +15,43 @@ public class Quiz_1 {
     }
 
     public static boolean isAnagram(String str1, String str2) {
+        int[] alphabet= new int[26];
+
+        // 입력값에 대한 처리
+        if(str1==null || str2==null ) {
+            return false;
+        }
+        if (str1.isEmpty() || str2.isEmpty()) {
+            return false;
+        }
+        if (!str1.matches("[a-zA-Z\\s]+") || !str2.matches("[a-zA-Z\\s]+")) {
+            return false;
+        }
+
+        // 공백제거 및 소문자로 변경
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
 
         if (str1.length() != str2.length()) {
             return false;
         }
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
 
         char[] charArray1 = str1.toCharArray();
-        char[] charArray2 = str2.toCharArray();
-        Arrays.sort(charArray1);
-        Arrays.sort(charArray2);
+        for (char c : charArray1) {
+            alphabet[c - 'a']++;
+        }
 
-        return Arrays.equals(charArray1, charArray2);
+        char[] charArray2 = str2.toCharArray();
+        for (char c : charArray2) {
+            alphabet[c - 'a']--;
+        }
+
+        for( int i : alphabet) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
