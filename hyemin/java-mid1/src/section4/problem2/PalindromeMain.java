7744cfa66694855978a20b66261b619402473f12
hmkim199 <hmkim199@gmail.com>
2024-12-10 20:34:50 +0900

    refactor. 시간 복잡도 고려 by 양성빈

--- hyemin/java-mid1/src/section4/problem2/PalindromeMain.java
+++ hyemin/java-mid1/src/section4/problem2/PalindromeMain.java
@@ -3,7 +3,7 @@ package src.section4.problem2;
 public class PalindromeMain {
     public static void main(String[] args) {
         String input = "babad";
-        String longestPalindrome = Palindrome.longestPalindrome(input);
+        String longestPalindrome = LongestPalindrome.longestPalindrome(input);
         System.out.println(longestPalindrome); // bab
     }
 }
