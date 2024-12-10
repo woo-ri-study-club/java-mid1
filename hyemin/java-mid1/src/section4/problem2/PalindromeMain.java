9b2d1b325fd7ebfc504b1c2d7c661e0fac443c48
hmkim199 <hmkim199@gmail.com>
2024-12-10 20:50:23 +0900

    feat. 섹션4 - 문제 2: 다른 방식 구현 추가

--- hyemin/java-mid1/src/section4/problem2/PalindromeMain.java
+++ hyemin/java-mid1/src/section4/problem2/PalindromeMain.java
@@ -4,6 +4,8 @@ public class PalindromeMain {
     public static void main(String[] args) {
         String input = "babad";
         String longestPalindrome = LongestPalindrome.longestPalindrome(input);
+        String longestPalindrome2 = LongestPalindrome2.longestPalindrome(input);
         System.out.println(longestPalindrome); // bab
+        System.out.println(longestPalindrome2); // bab
     }
 }
