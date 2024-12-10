2f2cb0755cd268069152dec115be19908c51a49f
nahye <nnhh.shine@gmail.com>
2024-12-10 20:38:02 +0900

    fix. 미션 4의 문제 2번 수정 및 피드백 반영 by 성빈님
    - extractPalindrome() 메소드 팰린드롬인 경우만 반환하도록 수정 및 단어 중간부터 양옆으로 검사하도록 수정
    - findLongestPalindrome() 단어길이가 홀수일때 짝수일때로 수정

--- nahye/java-mid1/src/section04/Quiz_2.java
+++ nahye/java-mid1/src/section04/Quiz_2.java
@@ -6,36 +6,37 @@ public class Quiz_2 {
         System.out.println(findLongestPalindrome(input));
 
     }
+
     public static String findLongestPalindrome(String input) {
         if (input == null || input.length() < 2) {
             return input;
         }
 
-        String longest = "";
+        String longest = input.substring(0, 1);
 
-       for(int i = 0; i < input.length(); i++) {
-           for (int j = i + 1; j < input.length(); j++) {
-               String result = extractPalindrome(input, i, j);
-               if (result.length() > longest.length()) {
-                   longest = result;
-               }
-           }
-       }
+        for (int i = 0; i < input.length(); i++) {
+
+            // 단어의 길이가 홀수일 때
+            String oddStr = extractPalindrome(input, i, i);
+            if (oddStr.length() > longest.length()) {
+                longest = oddStr;
+            }
+
+            // 단어의 길이가 짝수일 때
+            String evenStr = extractPalindrome(input, i, i + 1);
+            if (evenStr.length() > longest.length()) {
+                longest = evenStr;
+            }
+        }
+
         return longest;
     }
 
-    public static String extractPalindrome(String input, int start, int end) {
-        int left = start;
-        int right = end;
-
-        while (left < right) {
-            if (input.charAt(left) != input.charAt(right)) {
-                return "";
-            }
-            left++;
-            right--;
+    public static String extractPalindrome(String input, int left, int right) {
+        while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
+            left--;
+            right++;
         }
-
-        return input.substring(start, end + 1);
+        return input.substring(left + 1, right);
     }
 }
