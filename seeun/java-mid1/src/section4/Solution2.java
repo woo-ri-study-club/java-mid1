593b9a91dcb0d92901de5c6570ed5f805839d906
tdronnie <senyvee9@gmail.com>
2024-12-10 10:20:04 +0900

    refactor. 전역 변수 제거, 메서드명 수정 by 성빈님

--- seeun/java-mid1/src/section4/Solution2.java
+++ seeun/java-mid1/src/section4/Solution2.java
@@ -5,42 +5,50 @@ public class Solution2 {
     public static void main(String[] args) {
 
         String input = "babad";
-        System.out.println(findLongestPelindrome(input));
+        System.out.println(findLongestPalindrome(input));
     }
 
-    static int maxStart = 0;
-    static int maxLen = 0;
 
-    private static String findLongestPelindrome(String input) {
+
+    private static String findLongestPalindrome(String input) {
         if(input.isEmpty() || input.length() == 1){
             return input;
         }
 
         int len = input.length();
+        int maxStart = 0;
+        int maxLen = 0;
 
         for(int center = 0; center<len; center++){
             int left = center;
             int right = center;
-            findPelindrome(len, input, left, right);
+            int[] result = findPalindrome(input, left, right);
+            if(maxLen < result[1]){
+                maxLen = result[1];
+                maxStart = result[0];
+            }
 
             left = center;
             right = center + 1;
-            findPelindrome(len, input, left, right);
+            result = findPalindrome(input, left, right);
+            if(maxLen < result[1]){
+                maxLen = result[1];
+                maxStart = result[0];
+            }
 
         }
 
         return input.substring(maxStart, maxStart + maxLen);
     }
 
-    private static void findPelindrome(int len, String input, int left, int right) {
-        while (left >= 0 && right < len && input.charAt(left) == input.charAt(right)) {
-            int currLen = right - left + 1;
-            if (currLen > maxLen) {
-                maxStart = left;
-                maxLen = currLen;
-            }
+    private static int[] findPalindrome(String input, int left, int right) {
+        while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
             left--;
             right++;
         }
+
+        left++;
+        right--;
+        return new int[]{left, right - left + 1};
     }
 }
