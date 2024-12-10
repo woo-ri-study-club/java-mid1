16b9f1acd9d15d32ed019c01535e418f10a21b0f
Sangmin <sm9171@nate.com>
2024-12-10 20:28:42 +0900

    fix. 섹션4의 미션1 수정 - 대소문자, 빈칸 무시 - 배열에 저장하여 비교하는 방식에서 map에 저장하여 비교하는 방식으로 수정

--- sangmin/java-mid1/src/main/java/section4/mission1/StringUtils.java
+++ sangmin/java-mid1/src/main/java/section4/mission1/StringUtils.java
@@ -1,27 +1,37 @@
 package section4.mission1;
 
+import java.util.HashMap;
+
 public class StringUtils {
     private StringUtils() {
     }
 
     public static boolean isAnagram(String str1, String str2) {
+        str1 = getIgnoreCaseString(str1);
+        str2 = getIgnoreCaseString(str2);
+
         if (notEqualsStringLength(str1, str2)) {
             return false;
         }
 
-        int[] count = new int[256];
+        HashMap<Character, Integer> map = new HashMap<>();
+
         for (int i = 0; i < str1.length(); i++) {
-            count[str1.charAt(i)]++;
-            count[str2.charAt(i)]--;
+            map.put(str1.charAt(i), map.getOrDefault(str1.charAt(i), 0) + 1);
+            map.put(str2.charAt(i), map.getOrDefault(str2.charAt(i), 0) - 1);
         }
 
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] != 0) {
+        for (int value : map.values()) {
+            if (value != 0) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private static String getIgnoreCaseString(String str1) {
+        return str1.toLowerCase().replaceAll("\\s", "");
     }
 
     private static boolean equalsStringLength(final String str1, final String str2) {
