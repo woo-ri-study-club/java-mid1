19fda4224cfab9055d2b91d899fdbc816ae9842d
SungbinYang <ysb05222@naver.com>
2024-12-10 20:54:54 +0900

    fix. 1차 코드리뷰 by 세은님

--- sungbin/java-mid1/src/section4/question2/LongestPalindrome.java
+++ sungbin/java-mid1/src/section4/question2/LongestPalindrome.java
@@ -7,21 +7,21 @@ public class LongestPalindrome {
             return "";
         }
 
-        int start = 0;
-        int end = 0;
+        int longestStart = 0;
+        int longestEnd = 0;
 
         for (int i = 0; i < s.length(); i++) {
             int len1 = expandAroundCenter(s, i, i); // 홀수길이
             int len2 = expandAroundCenter(s, i, i + 1); // 짝수길이
             int len = Math.max(len1, len2); // 최대 길이
 
-            if (len > (end - start)) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+            if (len > (longestEnd - longestStart)) {
+                longestStart = i - (len - 1) / 2;
+                longestEnd = i + len / 2;
             }
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(longestStart, longestEnd + 1);
     }
 
     private static int expandAroundCenter(String s, int left, int right) {
