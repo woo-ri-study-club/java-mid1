d565b9a1d0808fb7c9cadbffc83b65610f48c62b
tdronnie <senyvee9@gmail.com>
2024-12-15 16:50:10 +0900

    refactor. TransactionType 메서드 추출

--- seeun/java-mid1/src/section6/domain/TransactionType.java
+++ seeun/java-mid1/src/section6/domain/TransactionType.java
@@ -4,56 +4,26 @@ public enum TransactionType {
     DEPOSIT("입금") {
         @Override
         public void execute(Account sender, Account receiver, int amount) {
-            if(sender == null || receiver == null){
-                throw new IllegalArgumentException("잘못된 입출금 계좌입니다.");
-            }
-            if (sender.getStatus() != AccountStatus.ACTIVE) {
-                throw new IllegalStateException("휴면처리 되었거나 차단된 계좌입니다.");
-            }
-            if(amount <= 0){
-                throw new IllegalArgumentException("1원 이상 입금가능합니다.");
-            }
+            verifySingleTransaction(sender);
+            verifyAmount(amount);
             sender.deposit(amount);
         }
     },
     WITHDRAW("출금") {
         @Override
         public void execute(Account sender, Account receiver, int amount) {
-            if(sender == null || receiver == null){
-                throw new IllegalArgumentException("잘못된 입출금 계좌입니다.");
-            }
-            if (sender.getStatus() != AccountStatus.ACTIVE) {
-                throw new IllegalStateException("휴면처리 되었거나 차단된 계좌입니다.");
-            }
-            if(amount <= 0){
-                throw new IllegalArgumentException("1원 이상 출금가능합니다.");
-            }
-            if(sender.getBalance() < amount){
-                throw new IllegalStateException("잔액이 부족합니다.");
-            }
-
+            verifySingleTransaction(sender);
+            verifyAmount(amount);
+            verifyBalance(sender, amount);
             sender.withdraw(amount);
         }
     },
     TRANSFER("송금") {
         @Override
         public void execute(Account sender, Account receiver, int amount) {
-            if(sender == null || receiver == null){
-                throw new IllegalArgumentException("잘못된 입출금 계좌입니다.");
-            }
-            if (sender.getStatus() != AccountStatus.ACTIVE) {
-                throw new IllegalStateException("휴면처리 되었거나 차단된 계좌에서 송금은 불가합니다.");
-            }
-            if (receiver.getStatus() != AccountStatus.ACTIVE) {
-                throw new IllegalStateException("휴면처리 되었거나 차단된 계좌로의 송금은 불가합니다.");
-            }
-            if(amount <= 0){
-                throw new IllegalArgumentException("1원 이상 송금가능합니다.");
-            }
-            if (sender.getBalance() < amount) {
-                throw new IllegalStateException("잔액이 부족합니다.");
-            }
-
+            verifyCrossTransaction(sender, receiver);
+            verifyAmount(amount);
+            verifyBalance(sender, amount);
             sender.withdraw(amount);
             receiver.deposit(amount);
         }
@@ -67,4 +37,40 @@ public enum TransactionType {
 
     public abstract void execute(Account sender, Account receiver, int amount);
 
+    protected void verifySingleTransaction(Account account){
+        verifyAccountExists(account);
+        verifyAccountStatus(account);
+    }
+
+    protected void verifyCrossTransaction(Account sender, Account receiver) {
+        verifyAccountExists(sender);
+        verifyAccountExists(receiver);
+        verifyAccountStatus(sender);
+        verifyAccountStatus(receiver);
+
+    }
+
+    protected void verifyAccountExists(Account account) {
+        if (account == null) {
+            throw new IllegalArgumentException("잘못된 입출금 계좌입니다.");
+        }
+    }
+
+    protected void verifyAccountStatus(Account account) {
+        if (account.getStatus() != AccountStatus.ACTIVE) {
+            throw new IllegalStateException("휴면처리 되었거나 차단된 계좌는 거래가 불가능합니다.");
+        }
+    }
+
+    protected void verifyBalance(Account account, int amount) {
+        if (account.getBalance() < amount) {
+            throw new IllegalStateException("잔액이 부족합니다.");
+        }
+    }
+
+    protected void verifyAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("1원 이상 거래 가능합니다.");
+        }
+    }
 }
